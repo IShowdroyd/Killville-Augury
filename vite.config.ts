3b1900330bee2d7ceabca7510c0b02c40049c5e0
IShowdroyd <ishowdroyd@gmail.com>
2025-08-22 19:02:26 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/KillVille-Augury/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Killville-Augury/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
